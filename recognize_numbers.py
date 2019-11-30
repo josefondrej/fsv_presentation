5255075d1edb685b468d96f0aa60077b94ed9950
Josef-Ondrej <josef.ondrej@ibm.com>
2019-11-30 13:17:03 +0100

    Add Recognize 3 from 4 notebook

--- recognize_numbers.py
+++ recognize_numbers.py
@@ -9,7 +9,7 @@ ROWS, COLS = SHAPE
 GRAYSCALE_RANGE = 255
 
 
-def load_data(n_datapoints: int = 60000) -> List[Tuple[np.ndarray, int]]:
+def load_data(n_datapoints: int = 6000) -> List[Tuple[np.ndarray, int]]:
     data = []
 
     mnist_train = tfds.load(name="mnist", split="train")
@@ -18,13 +18,21 @@ def load_data(n_datapoints: int = 60000) -> List[Tuple[np.ndarray, int]]:
         image, label = mnist_example["image"], mnist_example["label"]
         image = image.numpy()[:, :, 0].astype(np.float32) / GRAYSCALE_RANGE
         label = (int)(label)
-        if label in [0, 1]:
+        if label in [3, 4]:
             data.append((image, label))
 
     return data
 
 
+def predict(mask: np.ndarray, image: np.ndarray) -> int:
+    score = np.average(image * mask)
+    if score > 0:
+        return 3
+    else:
+        return 4
+
 def plot_image(image: np.ndarray):
+    plt.figure(figsize=(15, 15))
     plt.imshow(image, cmap="gray_r")
     plt.colorbar()
     ax = plt.axes()
@@ -51,10 +59,8 @@ def evaluate_mask(mask: np.ndarray, data: List[Tuple[np.ndarray, int]]):
     correct_predictions = 0
     for datapoint in data:
         image, label = datapoint
-        score = np.sum(image * mask)
-        predicted_zero = score > 0
-        actual_zero = (label == 0)
-        if predicted_zero == actual_zero:
+        prediction = predict(mask, image)
+        if prediction == label:
             correct_predictions += 1
 
     return correct_predictions / len(data)
@@ -75,7 +81,7 @@ def cross_masks(mother: np.ndarray, father: np.ndarray) -> np.ndarray:
 
 
 def get_random_mask_based_on_scores(mask_pool: List[np.ndarray], scores: List[float]) -> np.ndarray:
-    probas = np.array(scores)
+    probas = np.array(scores) ** 2
     probas = probas / np.sum(probas)
     index = np.where(np.random.multinomial(1, probas))[0][0]
     return mask_pool[index]
@@ -91,44 +97,38 @@ def get_best_mask(mask_pool: List[np.ndarray], scores: List[float]) -> np.ndarra
     return best_mask
 
 
+def plot_data_sample(data: List[np.ndarray], predictions: List[int] = None, cols: int = 8):
+    total = len(data)
+    rows = total // cols
+    if rows * cols > total:
+        rows += 1
+    fig, axes = plt.subplots(nrows=rows, ncols=cols, figsize=(20, 7))
+    for i in range(cols * rows):
+        if i < len(data):
+            subplot = axes[i // cols, i % cols]
+            subplot.imshow(data[i], cmap="gray_r")
+            subplot.axis("off")
+            if predictions is not None:
+                subplot.set_title(f"{predictions[i]}")
+        else:
+            subplot.axis("off")
+
 if __name__ == "__main__":
     data_size = 60000
-
-    new_generation_size = 60
-    old_generation_size = 30
-    alien_size = 10
-
-    epochs = 10000
-    print_frequency = 50
-
-    mask_pool_size = old_generation_size + new_generation_size + alien_size
+    mask_pool_size = 10000
 
     data = load_data(data_size)
 
     mask_pool = [generate_random_mask() for i in range(mask_pool_size)]
+    mask_scores = [evaluate_mask(mask, data) for mask in mask_pool]
 
-    for i in range(epochs):
-        mask_scores = [evaluate_mask(mask, data) for mask in mask_pool]
-        new_mask_pool = []
+    good_masks = [mask for mask, score in zip(mask_pool, mask_scores) if score > 0.9]
+    average_mask = np.average(good_masks)
 
-        for j in range(new_generation_size):
-            mother = get_random_mask_based_on_scores(mask_pool, mask_scores)
-            father = get_random_mask_based_on_scores(mask_pool, mask_scores)
-            child = cross_masks(mother, father)
-            new_mask_pool.append(child)
+    plot_image(average_mask)
 
-        for j in range(old_generation_size):
-            old_guy = get_random_mask_based_on_scores(mask_pool, mask_scores)
-            new_mask_pool.append(old_guy)
-
-        for j in range(alien_size):
-            alien = generate_random_mask()
-            new_mask_pool.append(alien)
-
-        if i % print_frequency == 0:
-            best_mask = get_best_mask(mask_pool, mask_scores)
-            best_mask_score = evaluate_mask(best_mask, data)
-            plt.title(f"Best mask, correct percentage: {best_mask_score:.3}")
-            plot_image(best_mask)
-
-        mask_pool = new_mask_pool
+    # best_mask = get_best_mask(mask_pool, mask_scores)
+    # best_mask_score = evaluate_mask(best_mask, data)
+    #
+    # plt.title(f"Best mask, correct percentage: {best_mask_score:.3f}")
+    # plot_image(best_mask)
